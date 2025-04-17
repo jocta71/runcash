6d30acb126c470e16c510d2a821df9b8bea99b0d
Jocta <joctaxd@gmail.com>
2025-04-17 20:09:28 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 8080,
-    host: true,
-  },
-});
+}));
