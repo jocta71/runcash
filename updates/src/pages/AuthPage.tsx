3f94562d497318d44f05c1f369d75a6358e6de60
Jocta <joctaxd@gmail.com>
2025-04-17 19:35:42 -0300

    assss

--- updates/src/pages/AuthPage.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function AuthPage() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Login / Registro</h1>
-    </div>
-  );
-}
--- updates/src/pages/BillingPage.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function BillingPage() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Faturamento</h1>
-    </div>
-  );
-}
